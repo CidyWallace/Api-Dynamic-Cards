fd42c11888d2ff011cfb632441554ed290e8c8ce
cidy <cidywallace12@gmail.com>
2026-05-19 09:44:23 -0300

    first commit

---
+++ src/main/java/ufpb/dcx/api_dynamic_cards/ApiDynamicCardsApplication.java
@@ -0,0 +1,13 @@
+package ufpb.dcx.api_dynamic_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDynamicCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDynamicCardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufpb/dcx/api_dynamic_cards/ApiDynamicCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.dcx.api_dynamic_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDynamicCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
